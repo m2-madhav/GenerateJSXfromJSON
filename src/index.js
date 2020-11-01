0a6817a95e195576c4cc6e56fe9e7c621b445b9e
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-11-01 21:39:44 +0530

    nothing changed

--- src/index.js
+++ src/index.js
@@ -1,51 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-function generateCodeFromObject(obj){
-    let ans_str="<";
-    ans_str+=obj.name+"";
-    if(obj.style!==undefined && Object.keys(obj.style).length>0){
-        ans_str+=" style{{";
-        let style_keys=Object.keys(obj.style);
-        for(let i=0;i<Object.keys(obj.style).length;i++){
-            ans_str+=camelize(style_keys[i])+':"'+obj.style[style_keys[i]]+'",';
-        }
-        ans_str=ans_str.substr(0,ans_str.length-1)+"}}";
+
+function generateCodeFromObject(obj) {
+  let ans_str = "<";
+  ans_str += obj.name + "";
+  if (obj.style !== undefined && Object.keys(obj.style).length > 0) {
+    ans_str += " style{{";
+    let style_keys = Object.keys(obj.style);
+    for (let i = 0; i < Object.keys(obj.style).length; i++) {
+      ans_str +=
+        camelize(style_keys[i]) + ':"' + obj.style[style_keys[i]] + '",';
     }
-    //console.log(obj.children!=undefined && obj.children.length>0);
-    if(obj.children!==undefined && obj.children.length>0){
-        ans_str+=">\n";
-        obj.children.forEach((ele)=>{
-            ans_str+=generateCodeFromObject(ele);
-        });
-        ans_str+="</"+obj.name+">\n";
-    }
-    else{
-        ans_str+=">\n";
-    }
-    return ans_str;
-    //return a code generated string
-   }
-// const Title=(str)=>{
-//     return str.replace(
-//         /\w\s*/g,
-//         function(text){
-//             return text.charAt(0).toUpperCase()+text.substr(1,text.length).toLowerCase();
-//         }
-//     ).replace(" ","");
-// }
-camelize = function camelize(str) {
-    return str.replace(/\W+(.)/g, function(match, chr)
-     {
-          return chr.toUpperCase();
-      });
+    ans_str = ans_str.substr(0, ans_str.length - 1) + "}}";
   }
-// const camleCase=(str)=>{
-//     str= str.replace("-"," ")
-//               .replace(/\s(.)/g,function(text){return text.toUpperCase();})
-//               .replace(/\s/g,"")
-//               .replace(/^(.)/g,function(text){return text.toLowerCase();});
-//     return str;
-// }
-   
-module.exports=generateCodeFromObject;
+  if (obj.children !== undefined && obj.children.length > 0) {
+    ans_str += ">\n";
+    obj.children.forEach((ele) => {
+      ans_str += generateCodeFromObject(ele);
+    });
+    ans_str += "</" + obj.name + ">\n";
+  } else {
+    ans_str += ">\n";
+  }
+  return ans_str;
+}
+
+function camelize(str) {
+  return str.replace(/\W+(.)/g, function (match, chr) {
+    return chr.toUpperCase();
+  });
+}
+
+module.exports = generateCodeFromObject;
